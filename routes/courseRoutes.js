5a416e829a7c574d01b0e314083616e38614d49e
Rislakp <rizlaah01@gmail.com>
2026-08-07 12:34:42 +0530

    delete,put api

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -6,4 +6,8 @@ router.get('/', courseController.getCourses);
 
 router.post('/', courseController.createCourse);
 
+router.put('/:courseId', courseController.updateCourse);
+
+router.delete('/:courseId', courseController.deleteCourse);
+
 module.exports = router;
