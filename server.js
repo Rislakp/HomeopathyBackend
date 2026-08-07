acf16a0f1976aece6003ed3c7554be059618e84e
Rislakp <rizlaah01@gmail.com>
2026-08-07 13:30:45 +0530

    update

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 const app = express();
+
+// CORS
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(express.json());
 
 // Routes
 app.use('/api/courses', require('./routes/courseRoutes'));
 
-// MongoDB connection
+// MongoDB
 connectDB();
 
+// Test
 app.get('/', (req, res) => {
   res.json({
-    message: 'Backend is working'
+    success: true,
+    message: 'Backend is working',
   });
 });
 
